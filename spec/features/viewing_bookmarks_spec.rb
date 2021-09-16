228989797a8230ac6fdb9045255e546b11e0f1b2
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-16 14:29:43 +0100

    Committing before oving repo

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -10,5 +10,6 @@ feature 'Viewing the bookmarks' do
   scenario 'can see the bookmarks listed on the page' do
     visit('/bookmarks')
     expect(page).to have_content('http://www.makersacademy.com')
+    expect(page).to have_content('http://www.twitter.com')
   end
 end
