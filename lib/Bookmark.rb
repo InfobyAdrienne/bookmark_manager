e5a646d99634434b75eec1b5a5176546ebf16510
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-16 11:49:14 +0100

    Interacting with PostgreSQL from Ruby

--- lib/Bookmark.rb
+++ lib/Bookmark.rb
@@ -1,10 +1,15 @@
+require 'pg'
+
 class Bookmark
     def self.all
-      [
-        "http://www.makersacademy.com",
-        "https://www.cosmopolitan.com/",
-        "https://graziadaily.co.uk/",
-        "https://www.complex.com/uk"
-       ]
+      connection = PG.connect(dbname: 'bookmark_manager')
+      result = connection.exec('SELECT * FROM bookmarks')
+      result.map { |bookmark| bookmark['url'] }
+    #   [
+    #     "http://www.makersacademy.com",
+    #     "https://www.cosmopolitan.com/",
+    #     "https://graziadaily.co.uk/",
+    #     "https://www.complex.com/uk"
+    #    ]
     end
   end
--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -3,14 +3,19 @@ require_relative '../../lib/bookmark'
 # As a user that works with many website  
 # so that I can find a saved website easly  
 # I want my app to show me a list of bookmarks
+
 describe Bookmark do 
-  describe 'all' do 
+  describe '.all' do 
     it 'shows a list of all the bookmarks' do 
     bookmarks = Bookmark.all
     expect(bookmarks).to include("http://www.makersacademy.com")
-    expect(bookmarks).to include("https://www.cosmopolitan.com/")
-    expect(bookmarks).to include("https://graziadaily.co.uk/")
-    expect(bookmarks).to include("https://www.complex.com/uk")
+    expect(bookmarks).to include("http://www.google.com")
+    expect(bookmarks).to include("http://www.destroyallsoftware.com")
+
+    # Other sites left here for testing SQL in terminal: 
+    # expect(bookmarks).to include("https://www.cosmopolitan.com/")
+    # expect(bookmarks).to include("https://graziadaily.co.uk/")
+    # expect(bookmarks).to include("https://www.complex.com/uk")
     end 
   end 
 end
